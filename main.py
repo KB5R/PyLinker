792ebfe245deadaa5cd348610237aa4febfbf650
Mihail Popkov <mihail.ku.88@gmail.com>
2025-05-26 16:00:48 +0300

    Connect to hosts main.py

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import subprocess
-import sys
 import logging
-import signal
 
-def inteactive_session_ssh():
+def inteactive_session_ssh(host, user, port):
     try:
         cmd = ["ssh", f"{user}@{host}", "-p", str(port)]
         subprocess.run(cmd, check=True)
@@ -14,7 +12,18 @@ def inteactive_session_ssh():
         logging.error(f"SSH failed with error: {e}")
     except Exception as e:
         logging.error(f"Unexpected error occurred: {e}")
+
 def main():
-    print("123")
+    host = input("Enter destination hosts:")
+    user = input("Enter name users: ")
+    port_input = input("Enter port [default 22]: ").strip()
+    if port_input:
+        port = int(port_input)
+    else:
+        port = 22
+    print("Entered hosts", host)
+    print("Entered user", user)
+    print("Entered ports", port)
+    inteactive_session_ssh(host, user, port)
 if __name__ == "__main__":
     main()
